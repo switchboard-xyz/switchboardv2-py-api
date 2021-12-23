3d3b288a3d6949f7cf9f743710c4f6778ddcfe14
mgild <mgild@users.noreply.github.com>
2021-12-23 09:10:12 -0500

    init impl skeleton

---
+++ lib.py
@@ -0,0 +1,14 @@
+import solana
+import anchor
+
+
+class AggregatorAccount:
+
+    def __init__(program, pubkey):
+        pass
+
+    def loadData():
+        pass
+
+    def create(aggregatorInitParams):
+        pass
