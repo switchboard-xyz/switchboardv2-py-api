587738d68c7ffd77b301d450616418b7e76e21e1
Albert Hermida <alberthermida@gmail.com>
2022-01-12 00:29:33 -0500

    1: Flesh out LeaseAccount logic - will need some thorough testing

--- switchboardpy/lease.py
+++ switchboardpy/lease.py
@@ -6,16 +6,19 @@ from typing import TYPE_CHECKING
 from dataclasses import dataclass
 
 from decimal import Decimal
-from solana import publickey
+from solana import publickey, system_program
 from solana.keypair import Keypair
 from solana.publickey import PublicKey
+from spl.token.constants import ASSOCIATED_TOKEN_PROGRAM_ID, TOKEN_PROGRAM_ID
+from spl.token.instructions import get_associated_token_address
 
 from switchboardpy.oraclequeue import OracleQueueAccount
 from switchboardpy.common import AccountParams
 
-
 if TYPE_CHECKING:
     from switchboardpy.aggregator import AggregatorAccount
+    from switchboardpy.program import ProgramStateAccount
+
 
 # Parameters for initializing a LeaseAccount
 @dataclass
@@ -137,3 +140,147 @@ class LeaseAccount:
         )
     
         return LeaseAccount(AccountParams(program=program, public_key=pubkey)), bump
+
+    """
+    Create and initialize the LeaseAccount.
+
+    Args:
+        program (anchor.Program): Switchboard program representation holding connection and IDL.
+        params (LeaseInitParams)
+    
+    Returns:
+        LeaseAccount
+    """
+    @staticmethod
+    async def create(program: anchorpy.Program, params: LeaseInitParams):
+        program_state_account, state_bump = ProgramStateAccount.from_seed(program)
+        switch_token_mint = await program_state_account.get_token_mint()
+        lease_account, lease_bump = LeaseAccount.from_seed(
+            program,
+            params.oracle_queue_account,
+            params.aggregator_account
+        )
+        escrow = get_associated_token_address(
+            lease_account.public_key,
+            switch_token_mint.program_id
+        )
+
+        try:
+            await switch_token_mint.create_associated_token_account(lease_account.public_key)
+        except Exception:
+            pass
+        await program.rpc["lease_init"](
+            {
+                "load_amount": params.load_amount,
+                "state_bump": state_bump,
+                "lease_bump": lease_bump,
+                "withdraw_authority": params.withdraw_authority or PublicKey('11111111111111111111111111111111')
+            },
+            ctx=anchorpy.Context(
+                accounts={
+                    "program_state": program_state_account.public_key,
+                    "lease": lease_account.public_key,
+                    "queue": params.oracle_queue_account.public_key,
+                    "aggregator": params.aggregator_account.public_key,
+                    "system_program": system_program.SYS_PROGRAM_ID,
+                    "funder": params.funder,
+                    "payer": program.provider.wallet.public_key,
+                    "token_program": TOKEN_PROGRAM_ID,
+                    "escrow": escrow,
+                    "owner": params.funder_authority.public_key,
+                },
+                signers=[params.funder_authority]
+            )
+        )
+        return LeaseAccount(AccountParams(program=program, public_key=lease_account.public_key))
+
+    """
+    Adds fund to a LeaseAccount. Note that funds can always be withdrawn by
+    the withdraw authority if one was set on lease initialization.
+
+    Args:
+        program (anchor.Program): Switchboard program representation holding connection and IDL.
+        params (LeaseExtendParams)
+    
+    Returns:
+        TransactionSignature
+    """
+    async def extend(self, params: LeaseExtendParams):
+        program = self.program
+        lease = await self.load_data()
+        escrow = lease.escrow
+        queue = lease.queue
+        aggregator = lease.aggregator
+        program_state_account, state_bump = ProgramStateAccount.from_seed(program)
+        switch_token_mint = await program_state_account.get_token_mint()
+        lease_account, lease_bump = LeaseAccount.from_seed(
+            program,
+            OracleQueueAccount(AccountParams(program=program, public_key=queue)),
+            AggregatorAccount(AccountParams(program=program, public_key=aggregator))
+        )
+        return await program.rpc["lease_extend"](
+            {
+                "load_amount": params.load_amount,
+                "state_bump": state_bump,
+                "lease_bump": lease_bump
+            },
+            ctx=anchorpy.Context(
+                accounts={
+                    "lease": lease_account.public_key,
+                    "aggregator": aggregator,
+                    "queue": queue,
+                    "funder": params.funder,
+                    "owner": params.funder_authority.public_key,
+                    "token_program": TOKEN_PROGRAM_ID,
+                    "escrow": escrow,
+                    "program_state": program_state_account.public_key
+                },
+                signers=[params.funder_authority]
+            )
+        )
+ 
+    """
+    Withdraw stake and/or rewards from a LeaseAccount.
+
+    Args:
+        params (LeaseWithdrawParams)
+    
+    Returns:
+        TransactionSignature
+
+    Raises:
+        AccountDoesNotExistError: If the account doesn't exist.
+        AccountInvalidDiscriminator: If the discriminator doesn't match the IDL.
+    """
+    async def withdraw(self, params: LeaseWithdrawParams):
+        program = self.program
+        lease = await self.load_data()
+        escrow = lease.escrow
+        queue = lease.queue
+        aggregator = lease.aggregator
+        program_state_account, state_bump = ProgramStateAccount.from_seed(program)
+        lease_account, lease_bump = LeaseAccount.from_seed(
+            program,
+            OracleQueueAccount(AccountParams(program=program, public_key=queue)),
+            AggregatorAccount(AccountParams(program=program, public_key=aggregator))
+        )
+        return await self.program.rpc["lease_withdraw"](
+            {
+                "amount": params.amount,
+                "state_bump": state_bump,
+                "lease_bump": lease_bump
+            },
+            ctx=anchorpy.Context(
+                accounts={
+                    "lease": lease_account.public_key,
+                    "escrow": escrow,
+                    "aggregator": aggregator,
+                    "queue": queue,
+                    "withdraw_authority": params.withdraw_authority.public_key,
+                    "withdraw_account": params.withdraw_wallet,
+                    "token_program": TOKEN_PROGRAM_ID,
+                    "program_state": program_state_account.public_key,
+                },
+                signers=[params.withdraw_authority]
+            )
+        )
